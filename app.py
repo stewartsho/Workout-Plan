ee166bfdcd7e2954ea4aafa6d0b1180d2168fcd4
stewartsho <stewartsho@gmail.com>
2024-06-01 14:13:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ name = ""
 makersuite_api = os.getenv("MAKERSUITE_API_TOKEN")
 palm.configure(api_key=makersuite_api)
 
-model = {'model' : "models/chat-bison-001"}
 
+model = {"model" : "models/chat-bison-001"}
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
@@ -23,64 +23,48 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 def main():
     global flag, name
+    print("flag", flag)
     if flag == 1:
         name = request.form.get("q")
         current_time = datetime.datetime.now()
-        conn = sqlite3.connect('log.db')
+        conn = sqlite3.connect("log (1).db")
         c = conn.cursor()
-        c.execute("insert into user (name,time) values (?,?)" , (name,current_time))
+        c.execute("insert into user (name,time) values (?,?)",(name,current_time))
         conn.commit()
         c.close()
-        conn.close()       
+        conn.close()
         flag = 0
     return(render_template("main.html",r=name))
 
-@app.route("/prediction",methods=["GET","POST"])
-def prediction():
-    return(render_template("prediction.html",))
+@app.route("/workout",methods=["GET","POST"])
+def workout():
+    return(render_template("workout.html"))
 
-@app.route("/dbs_price",methods=["GET","POST"])
-def dbs_price():
-    s = float(request.form.get("q"))
-    return(render_template("dbs_price.html",r=(s*-50.6)+90.2))
-
-@app.route("/generate_text",methods=["GET","POST"])
-def generate_text():
-    return(render_template("generate_text.html"))
-
-@app.route("/text_result_makersuite",methods=["GET","POST"])
-def text_result_makersuite():
+@app.route("/workout_result",methods=["GET","POST"])
+def workout_result():
     q = request.form.get("q")
+    q = "workout pla for people with age " + q + " in table form"
     r = palm.chat(**model, messages=q)
-    return(render_template("text_result_makersuite.html",r=r.last))
+    return(render_template("workout_result.html",r=r.last))
 
-@app.route("/generate_image",methods=["GET","POST"])
-def generate_image():
-    return(render_template("generate_image.html"))
-
-@app.route("/image_result",methods=["GET","POST"])
-def image_result():
-    q = request.form.get("q")
-    r = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",input = {"prompt":q})    
-    return(render_template("image_result.html",r=r[0]))
 
 @app.route("/log",methods=["GET","POST"])
 def log():
-    conn = sqlite3.connect('log.db')
+    conn = sqlite3.connect("log (1).db")
     c = conn.cursor()
     c.execute("select * from user")
     r = ""
     for row in c:
-      r += str(row) + "<br>" 
+        r += str(row) + "<br>"
     print(r)
     r = Markup(r)
     c.close()
     conn.close()
-    return(render_template("log.html", r=r))
+    return(render_template("log.html",r=r))
 
 @app.route("/delete",methods=["GET","POST"])
 def delete():
-    conn = sqlite3.connect('log.db')
+    conn = sqlite3.connect('log (1).db')
     c = conn.cursor()
     c.execute("delete from user")
     conn.commit()
